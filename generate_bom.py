73d060f3c95036e3db74b284f1106a06beb02ecd
NZ design <contact@nzdesign.ch>
2026-02-10 09:24:22 +0100

    Update generate_bom.py

--- generate_bom.py
+++ generate_bom.py
@@ -6,7 +6,7 @@ import urllib.parse
 target_extension = ".stl"
 output_file = "BOM.md"
 base_folder = "stl"
-repo_url = "https://github.com/NZdesignch/asg29" # À MODIFIER
+repo_url = "https://github.com/NZdesignch" # À MODIFIER
 branch = "main"
 # ---------------------
 
